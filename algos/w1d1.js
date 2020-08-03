ed1d8d088d16963305683dd3f9545e5dd2bbb801
Benny Lin <bennylin@Bennys-MacBook-Air.local>
2020-08-03 12:25:14 -0700

    first commit

---
+++ algos/w1d1.js
@@ -0,0 +1,65 @@
+class SLL {
+    constructor() {
+        this.head = null;
+    }
+
+    isEmpty() {
+        return this.head === null;
+    }
+
+    addNodeToFront(node) {
+        if (this.head === null) {
+            this.head = node;
+        } else {
+            node.next = this.head;
+            this.head = node;
+        }
+    }
+
+    addDataToFront(data) {
+        if (this.head === null) {
+            this.head = new Node(data);
+        } else {
+            let newHead = new Node(data);
+            newHead.next = this.head;
+            this.head = newHead;
+        }
+    }
+
+    // printReverse() {
+    //     if (this.head === null) {
+    //         return null
+    //     } else {
+    //         this.next.printReverse()
+    //         console.log(this.data)
+    //     }
+    // } ------> recursive function not working
+}
+
+// if yous finish addToFront early, create a second version
+// that takes data rather than a node. -brendan
+
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+
+var myFirstSLL = new SLL();
+myFirstSLL.addNodeToFront(new Node(9));
+
+
+if(myFirstSLL.head){
+
+}
+// { head: {data: 7, next: {data: 8, next null}} }
+var myFirstNode = new Node(7);
+
+myFirstSLL.head = myFirstNode;
+myFirstSLL.head.next = new Node(8);
+myFirstSLL.addDataToFront(10)
+
+myFirstSLL.printReverse(myFirstSLL)
+
---
+++ intro/HelloCountry.java
@@ -0,0 +1,5 @@
+public class HelloCountry {
+    public static void printfn(String[] args) {
+        System.out.println("Hello Canada");
+    }
+}
