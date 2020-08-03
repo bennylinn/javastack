019d1819127a7041fdd4403079fda9b492620674
Benny Lin <bennylin@Bennys-MacBook-Air.local>
2020-08-03 12:45:43 -0700

    your own java program

---
+++ javaFun/Your Own Java Program/PrintPersonalInfo.java
@@ -0,0 +1,7 @@
+public class PrintPersonalInfo {
+    public static void main(String[] args) {
+        System.out.println("My name is " + args[0]);
+        System.out.println("I am " + args[1] + " years old");
+        System.out.println("My hometown is " + args[2]);
+    }
+}
