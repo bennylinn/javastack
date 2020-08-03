9098c9a7133d7672bdd2ad2927bc2448ab14281d
Benny Lin <bennylin@Bennys-MacBook-Air.local>
2020-08-03 16:20:11 -0700

    pythagoras assignment

---
+++ javaFun/Pythagorean Theorem/PythagoreanTest.java
@@ -0,0 +1,7 @@
+public class PythagoreanTest {
+    static public void main(String[] args) {
+        Pythagorean py = new Pythagorean();
+        double result = py.calculateHypotenuse(3, 4);
+        System.out.println("The result of 3, 4 is : " + result);
+    }
+}
